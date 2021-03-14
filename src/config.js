1388d682206a2b2875a792039dca771377c1eb57
Alex Muro <am3081@gmail.com>
2021-03-13 19:37:35 -0500

    test 123

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ export const PROJECT_THEME = 'TEST_THEME';
 
 let API_HOST = 'https://graph.availabs.org';
 let AUTH_HOST = 'https://availauth.availabs.org';
-let CLIENT_HOST = 'https://npmrds.availabs.org';
+let CLIENT_HOST = 'https://tds.availabs.org';
 
 if (process.env.NODE_ENV === 'development') {
   API_HOST = 'http://localhost:4444';
-  AUTH_HOST = 'http://localhost:3457';
+  //AUTH_HOST = 'http://localhost:3457';
   CLIENT_HOST = 'http://localhost:3000';
 }
 export { API_HOST, AUTH_HOST, CLIENT_HOST };
